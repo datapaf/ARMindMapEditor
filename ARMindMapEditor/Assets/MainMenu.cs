599a25b49bd0a2261bc61ec5add3b5c725765213
datapaf <georgyandryuschenko@gmail.com>
2020-06-12 19:14:15 +0300

    Implemented main menu

---
+++ ARMindMapEditor/Assets/MainMenu.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenu : MonoBehaviour
+{
+    private GameObject elements;
+
+    private void Start()
+    {
+        elements = transform.GetChild(0).gameObject;        
+    }
+
+    public void StartNewMap()
+    {
+        elements.SetActive(false);
+    }
+
+    public void Exit()
+    {
+        Application.Quit();
+    }
+}
